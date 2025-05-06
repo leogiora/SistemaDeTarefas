6c09107b344a4f854420af7524ce9f5bdf088bc2
leogiora <leogiora@hotmail.com>
2025-05-06 11:17:22 -0300

    refazer tudo

--- backend/src/main/java/com/example/backend/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.backend.repository;
-
-import com.exemplo.backend.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
