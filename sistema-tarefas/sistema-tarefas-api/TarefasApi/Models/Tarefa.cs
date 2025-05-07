a5f1e26ac6ebe6a0fe1fff6d47fb187c0498538d
leogiora <leogiora@hotmail.com>
2025-05-07 09:02:08 -0300

    ajustes no Program.cs e integração com Swagger finalizada

--- sistema-tarefas/sistema-tarefas-api/TarefasApi/Models/Tarefa.cs
+++ sistema-tarefas/sistema-tarefas-api/TarefasApi/Models/Tarefa.cs
@@ -3,7 +3,7 @@ namespace TarefasApi.Models
     public class Tarefa
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
+        public string Titulo { get; set; } = string.Empty;
         public string? Descricao { get; set; }
         public bool Concluida { get; set; } = false;
         public DateTime CriadaEm { get; set; } = DateTime.Now;
